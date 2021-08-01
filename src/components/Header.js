5e12b7947670027122d6165aaf111ed462927658
Remeedella <ship_jeremy@hotmail.com>
2021-08-01 18:52:15 -0400

    fe

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,11 +13,11 @@ export default function Header() {
             <div className="d-flex d-md-none">
               <div style={{width: '40%'}}>
               <Link to="/">
-                <StaticImage src="../images/logo.png" className="img-fluid ps-2 position-relative" style={{maxHeight: '180px', top: '8px'}} />
+                <StaticImage src="../images/logo.png" alt="logo" className="img-fluid ps-2 position-relative" style={{maxHeight: '180px', top: '8px'}} />
                 </Link>
               </div>
               <div className="align-self-end text-nowrap py-1 ps-1 d-inline-block" style={{width: '60%'}}>
-                <div className="slogan-xs"> 
+                
                   <svg width="100%" height="100%" viewBox="0 0 844 356" style={{fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin: 'round', strokeMiterlimit: 2}}>
                     <rect id="Artboard1" x={0} y={0} width="843.75" height="355.297" style={{fill: 'none'}} />
                     <text x="53.066px" y="225.807px" style={{fontFamily: '"BebasNeue-Regular", "Bebas Neue", sans-serif', fontSize: '146.767px'}}>T
@@ -27,13 +27,12 @@ export default function Header() {
                       <tspan x="97.559px 133.749px 169.178px 198.11px 234.859px 269.273px 301.555px 332.416px 364.495px 400.686px 428.653px 441.343px 476.315px 489.004px 518.749px 535.6px 569.862px 599.708px 636.456px 670.566px 701.427px 733.506px 769.696px " y="290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px ">HOTOGRAPHY &amp; VIDEOGAPHY</tspan>
                     </text>
                   </svg>
-                </div>
               </div>
             </div>
             <div className="d-none d-md-flex d-lg-none">
               <div style={{width: '25%'}}>
               <Link to="/">
-                <StaticImage src="../images/logo.png" className="img-fluid ps-2 position-relative" style={{maxHeight: '180px', top: '8px'}} />
+                <StaticImage src="../images/logo.png" alt="logo" className="img-fluid ps-2 position-relative" style={{maxHeight: '180px', top: '8px'}} />
                 </Link>
               </div>
               <div className="align-self-end text-nowrap py-1 ps-1 d-inline-block" style={{width: '38%'}}>
@@ -53,10 +52,10 @@ export default function Header() {
               </div>
               <nav className="bg-white d-flex mx-auto navbar navbar-light d-none d-md-flex align-items-center pt-3" style={{width: '35%', maxWidth: '400px'}}>
                 <div className="container">
-                  <StaticImage src="../images/instagram-ic.svg" className="img-fluid" />
-                  <StaticImage src="../images/facebook-ic.svg" className="img-fluid" />
-                  <StaticImage src="../images/youtube-ic.svg" className="img-fluid" />
-                  <StaticImage src="../images/fiverr-ic.svg" className="img-fluid" />
+                  <StaticImage src="../images/instagram-ic.svg" alt="instagram icon" className="img-fluid" />
+                  <StaticImage src="../images/facebook-ic.svg" alt="facebook icon" className="img-fluid" />
+                  <StaticImage src="../images/youtube-ic.svg" alt="youtube icon" className="img-fluid" />
+                  <StaticImage src="../images/fiverr-ic.svg" alt="fiverr icon" className="img-fluid" />
                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                   </button>
@@ -66,7 +65,7 @@ export default function Header() {
                         <Link to="/about/" className="nav-link" aria-current="page">about me</Link>
                       </li>
                       <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">galleries</a>
+                        <a className="nav-link dropdown-toggle" href="/" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">galleries</a>
                         <ul className="dropdown-menu" aria-labelledby="dropdown01">
                           <li>
                             <Link to="/galleries/lifestyle" className="dropdown-item">lifestyle</Link>
@@ -83,13 +82,13 @@ export default function Header() {
                         </ul>
                       </li>
                       <li className="nav-item">
-                        <a className="nav-link" href="#">video</a>
+                        <Link to="/" className="nav-link">video</Link>
                       </li>
                       <li className="nav-item">
-                        <a className="nav-link" href="#">blog</a>
+                        <Link to="/" className="nav-link">blog</Link>
                       </li>
                       <li className="nav-item">
-                        <a className="nav-link" href="#">contact</a>
+                        <Link to="/" className="nav-link">contact</Link>
                       </li>
                     </ul>
                   </div>
@@ -99,11 +98,10 @@ export default function Header() {
             <div className="d-none d-lg-flex d-xxl-one">
               <div style={{minWidth: '170px'}}>
               <Link to="/">
-                <StaticImage src="../images/logo.png" className="img-fluid ps-2 position-relative" style={{maxHeight: '180px', top: '8px'}} />
+                <StaticImage src="../images/logo.png" alt="logo" className="img-fluid ps-2 position-relative" style={{maxHeight: '180px', top: '8px'}} />
                 </Link>
               </div>
               <div className="align-self-end text-nowrap py-1 ps-1 d-inline-block" style={{maxWidth: '35%', paddingRight: '20px'}}>
-                <div className="slogan-xs"> 
                   <svg width="100%" height="100%" viewBox="0 0 844 356" style={{fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin: 'round', strokeMiterlimit: 2}}>
                     <rect id="Artboard1" x={0} y={0} width="843.75" height="355.297" style={{fill: 'none'}} />
                     <text x="53.066px" y="225.807px" style={{fontFamily: '"BebasNeue-Regular", "Bebas Neue", sans-serif', fontSize: '146.767px'}}>T
@@ -113,7 +111,6 @@ export default function Header() {
                       <tspan x="97.559px 133.749px 169.178px 198.11px 234.859px 269.273px 301.555px 332.416px 364.495px 400.686px 428.653px 441.343px 476.315px 489.004px 518.749px 535.6px 569.862px 599.708px 636.456px 670.566px 701.427px 733.506px 769.696px " y="290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px 290.078px ">HOTOGRAPHY &amp; VIDEOGAPHY</tspan>
                     </text>
                   </svg>
-                </div>
               </div>
               <div className="d-flex ms-auto text-nowrap align-self-end" style={{paddingBottom: '15px'}}>
                 <nav className="navbar navbar-expand-lg">
@@ -122,7 +119,7 @@ export default function Header() {
                     <Link to="/about/" className="nav-link" aria-current="page">about me</Link>
                     </li>
                     <li className="nav-item dropdown">
-                      <a className="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">galleries</a>
+                      <a className="nav-link dropdown-toggle" id="dropdown01" href="/" data-bs-toggle="dropdown" aria-expanded="false">galleries</a>
                       <ul className="dropdown-menu" aria-labelledby="dropdown01">
                       <li>
                             <Link to="/galleries/lifestyle" className="dropdown-item">lifestyle</Link>
@@ -139,53 +136,24 @@ export default function Header() {
                       </ul>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">video</a>
+                      <Link to="/" className="nav-link">video</Link>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">blog</a>
+                      <Link to="/" className="nav-link">blog</Link>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">contact</a>
+                      <Link to="/" className="nav-link">contact</Link>
                     </li>
                   </ul>
                 </nav>
               </div>
-              <nav className="align-items-end bg-white d-lg-flex d-none ms-auto navbar navbar-light" style={{width: '20%', paddingBottom: '25px', maxWidth: '300px'}}>
+              <nav className="align-items-end bg-white d-none d-lg-flex ms-auto navbar navbar-light" style={{width: '20%', paddingBottom: '25px', maxWidth: '300px'}}>
                 <div className="container" style={{maxWidth: '285px', minWidth: '154px'}}>
-                  <StaticImage src="../images/instagram-ic.svg" className="lg-icon" />
-                  <StaticImage src="../images/facebook-ic.svg" className="lg-icon" style={{maxHeight: '35px'}} />
-                  <StaticImage src="../images/youtube-ic.svg" className="lg-icon" style={{maxHeight: '35px'}} />
-                  <StaticImage src="../images/fiverr-ic.svg" className="lg-icon" style={{paddingTop: '3px'}} />
-                  <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-                    <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                      <li className="nav-item active">
-                      <Link to="/about/" className="nav-link" aria-current="page">about me</Link>
-                      </li>
-                      <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">galleries</a>
-                        <ul className="dropdown-menu" aria-labelledby="dropdown01">
-                          <li>
-                            <a className="dropdown-item" href="#">Action</a>
-                          </li>
-                          <li>
-                            <a className="dropdown-item" href="#">Another action</a>
-                          </li>
-                          <li>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="#">video</a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="#">blog</a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="#">contact</a>
-                      </li>
-                    </ul>
-                  </div>
+                  <StaticImage src="../images/instagram-ic.svg" alt="instagram icon" className="lg-icon" />
+                  <StaticImage src="../images/facebook-ic.svg" alt="facebook icon" className="lg-icon" style={{maxHeight: '35px'}} />
+                  <StaticImage src="../images/youtube-ic.svg" alt="youtube icon" className="lg-icon" style={{maxHeight: '35px'}} />
+                  <StaticImage src="../images/fiverr-ic.svg" alt="fiverr icon" className="lg-icon" style={{paddingTop: '3px'}} />
+                 
                 </div>
               </nav>
             </div>
@@ -205,7 +173,7 @@ export default function Header() {
                     <Link to="/about/" className="nav-link" aria-current="page">about me</Link>
                     </li>
                     <li className="nav-item dropdown">
-                      <a className="nav-link dropdown-toggle" href="#" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">galleries</a>
+                      <a className="nav-link dropdown-toggle" href="/" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">galleries</a>
                       <ul className="dropdown-menu" aria-labelledby="dropdown01">
                       <li>
                             <Link to="/galleries/lifestyle" className="dropdown-item">lifestyle</Link>
@@ -222,13 +190,13 @@ export default function Header() {
                       </ul>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">video</a>
+                      <Link to="/" className="nav-link">video</Link>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">blog</a>
+                      <Link to="/" className="nav-link">blog</Link>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">contact</a>
+                      <Link to="/" className="nav-link">contact</Link>
                     </li>
                   </ul>
                 </div>
--- src/pages/photos.js
+++ src/pages/photos.js
@@ -43,5 +43,40 @@ export const photos = [
     src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
     width: 4,
     height: 3
+  },
+  {
+    src: "https://source.unsplash.com/XiDA78wAZVw/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/x8xJpClTvR0/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/qGQNmBE7mYw/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/NuO6iTBkHxE/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/pF1ug8ysTtY/600x400",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/A-fubu9QJxE/800x533",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/5P91SF0zNsI/740x494",
+    width: 4,
+    height: 3
   }
 ];
