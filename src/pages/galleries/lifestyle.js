94171432e1d7743ea1578132fd3ebf6b4eabc06e
Remeedella <ship_jeremy@hotmail.com>
2021-08-06 14:05:45 -0400

    update lifestyle

--- src/pages/galleries/lifestyle.js
+++ src/pages/galleries/lifestyle.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "../../components/photos";
 import Layout from "../../components/Layout";
 import Header from "../../components/Header";
 import { Helmet } from "react-helmet";
 
 export default function lifestyle() {
- 
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
   return (
     <Layout>
       <Header />
@@ -16,6 +30,23 @@ export default function lifestyle() {
         <div className="title text-center display-1">
     lifestyle <span className="fs-2 text-secondary">gallery</span></div>
     <h2>some text to put about</h2>
+    <Gallery photos={photos} direction={"column"} onClick={openLightbox} />
+    <div>
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
     </main>
     </Layout>
   );
